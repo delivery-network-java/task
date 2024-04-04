9cd355931363491c8c6aafda2abcad05c8b01825
diego fernando muñoz ledesma <dfmunoz@indracompany.com>
2024-04-04 15:31:24 -0500

    respuesta a Issue #6 Crear clase de excepción personalizada

---
+++ src/Interfaces/Prioritizable.java
@@ -0,0 +1,9 @@
+package Interfaces;
+
+public interface Prioritizable {
+    // Método para establecer la prioridad de la tarea
+    void setPriority(int priority);
+
+    // Método para obtener la prioridad de la tarea
+    int getPriority();
+}
